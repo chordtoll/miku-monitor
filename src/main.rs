d63ae9b9102e93215bbbad83e53ed9154a240510
chordtoll <git@chordtoll.com>
2024-05-19 23:28:44 -0700

    slint

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,14 @@
 fn main() {
     println!("{}",String::from_utf8(libsqlite3_sys::SQLITE_VERSION.to_vec()).unwrap());
     println!("Hello, world!");
+    MainWindow::new().unwrap().run().unwrap();
+}
+
+slint::slint! {
+    export component MainWindow inherits Window {
+        Text {
+            text: "hello world";
+            color: green;
+        }
+    }
 }
